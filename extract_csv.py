b2834aa26bbf6266e581544e5df507b7779ecfd4
Aykerimm <69381281+Aykerimm@users.noreply.github.com>
2022-04-14 14:39:51 -0400

    Fixed STATUS column's data

--- extract_csv.py
+++ extract_csv.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 import pandas as pd
-
 YEAR = 365.2425
 
-data_folder = "./dataset"
+data_folder = "."
+#data_folder = "./dataset"
 
-application_record = pd.read_csv(f"{data_folder}/application_record.csv")
-credit_record = pd.read_csv(f"{data_folder}/credit_record.csv")
+application_record = pd.read_csv(f"{data_folder}/application_record.csv",index_col="ID")
+credit_record = pd.read_csv(f"{data_folder}/credit_record.csv", index_col="ID")
 
 # DAYS_BIRTH -> AGE
 
@@ -20,3 +20,15 @@ application_record.loc[application_record['YEARS_EMPLOYED'] <= 0, 'YEARS_EMPLOYE
 application_record.drop(columns="DAYS_EMPLOYED", axis=1, inplace=True)
 
 print(application_record.columns)
+
+df=pd.merge(application_record, credit_record, on='ID', how='left')
+df=df.drop_duplicates()
+
+
+# Replace X and C characters with -1
+df.STATUS = df.apply(lambda x: -1 if x.STATUS=='C' or x.STATUS == 'X' or pd.isna(x.STATUS) else x.STATUS, axis=1)
+# Change data type of STATUS column from string to integer
+df.STATUS = df.STATUS.astype('int')
+
+# Write merge result to the file
+df.to_csv('concatenated.csv')
