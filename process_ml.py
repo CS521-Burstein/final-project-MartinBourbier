c5214879bfa0219dfabc2760492a9037968f0b26
Martin Bourbier <martin.bourbier@epita.fr>
2022-05-09 21:50:19 -0400

    dropped some more values and save the model in a pickle file

--- process_ml.py
+++ process_ml.py
@@ -16,11 +16,12 @@ for label in data.columns.values:
     if data[label].dtype == 'object':
         data[label] = le.fit_transform(data[label])
 
+grid = None
 MODEL = "RFTC"
 
 # Logistic Regression model
 if MODEL == "LOGREG":
-    X = data.drop(['STATUS'], axis=1).drop(['ID'], axis=1)
+    X = data.drop(['STATUS', 'ID', 'Unnamed: 0'], axis=1)
     y = data['STATUS']
 
 
@@ -30,49 +31,49 @@ if MODEL == "LOGREG":
     X_train_rescaled = scaler.fit_transform(X_train)
     X_test_rescaled = scaler.transform(X_test)
 
-    LogReg = LogisticRegression(max_iter=100)
-    LogReg.fit(X_train_rescaled, y_train)
+    model = LogisticRegression(max_iter=100)
+    model.fit(X_train_rescaled, y_train)
 
     print("Logistic Regression model")
-    print("Accuracy before optimizations:", LogReg.score(X_test_rescaled, y_test))
+    print("Accuracy before optimizations:", model.score(X_test_rescaled, y_test))
 
     tolerance = [0.01, 0.001, 0.0001]
     max_iter = [100, 150, 200]
 
     params = dict(tol=tolerance, max_iter=max_iter)
 
-    model = GridSearchCV(estimator=LogReg, param_grid=params, cv=5)
+    grid = GridSearchCV(estimator=model, param_grid=params, cv=5)
 
     X_rescaled = scaler.fit_transform(X)
-    model_result = model.fit(X_rescaled, y)
+    model_result = grid.fit(X_rescaled, y)
 
     score, best_params = model_result.best_score_, model_result.best_params_
 
     print("Best params:", best_params, "with accuracy:", score)
 
-    LogReg.set_params(**best_params)
+    model.set_params(**best_params)
 
 # Decision Tree Classifier model
 elif MODEL == "DTC":
-    X = data.drop(['STATUS'], axis=1).drop(['ID'], axis=1)
+    X = data.drop(['STATUS', 'ID', 'Unnamed: 0'], axis=1)
     y = data['STATUS']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     scaler = MinMaxScaler(feature_range=(0, 1))
     
-    DT = DecisionTreeClassifier()
+    model = DecisionTreeClassifier()
     
-    DT.fit(X_train, y_train)
+    model.fit(X_train, y_train)
     
-    y_pred = DT.predict(X_test)
+    y_pred = model.predict(X_test)
 
     print("Decision Tree Classifier model")
     print("Accuracy:", accuracy_score(y_test, y_pred))
 
 # Random Forest Classifier model
 elif MODEL == "RFTC":
-    X = data.drop(['STATUS'], axis=1).drop(['ID'], axis=1)
+    X = data.drop(['STATUS', 'ID', 'Unnamed: 0'], axis=1)
     y = data['STATUS']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -80,14 +81,21 @@ elif MODEL == "RFTC":
     # scaler = MinMaxScaler(feature_range=(0, 1))
     # X_train_rescaled = scaler.fit_transform(X_train)
     # X_test_rescaled = scaler.transform(X_test)
-    RF = RandomForestClassifier()
+    model = RandomForestClassifier()
 
     # RF.fit(X_train_rescaled, y_train)
 
     # y_pred = RF.predict(X_test_rescaled)
-    RF.fit(X_train, y_train)
+    model.fit(X_train, y_train)
 
-    y_pred = RF.predict(X_test)
+    y_pred = model.predict(X_test)
 
     print("Random Forest Classifier")
     print("Accuracy:", round(accuracy_score(y_test, y_pred), 2))
+
+import pickle
+
+s = pickle.dumps(model)
+
+with open('model.pkl', 'wb') as f:
+    f.write(s)
