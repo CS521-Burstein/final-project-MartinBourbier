75e6f7cc241e0a9daa9ce323ccd6e73cf97f544a
Martin Bourbier <martin.bourbier@epita.fr>
2022-05-10 16:28:36 -0400

    wrong values handling

--- main.py
+++ main.py
@@ -9,23 +9,122 @@ with open('model.pkl', 'rb') as f:
 
 print("Welcome! Please enter your information below:")
 
+def amount_income():
+    while True:
+        try:
+            val = float(input("Enter your annual income: "))
+        except:
+            print("Please enter a valid input.")
+            continue
+        break
+    
+    return val
+
+def income_type():
+    while True:
+        answer = input("Enter your income type (Working/Commercial associate/Other): ")
+
+        if answer == "Working" or answer == "Commercial associate" or answer == "Other":
+            break
+
+        print("Please enter a valid input.")
+        
+    return answer
+
+def education_type():
+    while True:
+        answer = input("Enter your education type (Secondary/Higher education/Other): ")
+
+        if answer == "Secondary" or answer == "Higher education" or answer == "Other":
+            break
+        
+        print("Please enter a valid input.")
+        
+    return answer
+
+def family_status():
+    while True:
+        answer = input("Enter your family status (Single/Married/Civil Marriage/Separated/Other): ")
+
+        if answer == "Single" or answer == "Married" or answer == "Civil Marriage" or answer == "Separeted" or answer == "Other":
+            break
+        
+        print("Please enter a valid input.")
+        
+    return answer
+
+def housing_type():
+    while True:
+        answer = input("Enter your housing type ('House / apartment'/'Rented apartment'/'Municipal "
+                                "apartment'/'With parents'/Other): ")
+                            
+        if answer == "House / apartment" or answer == "Rented apartment" or answer == "Municipal apartment" or answer == "With parents" or answer == "Other":
+            break
+        
+        print("Please enter a valid input.")
+        
+    return answer
+
+def occupation_type():
+    while True:
+        answer = input("Enter your occupation type (Student/Professional/Other): ")
+        
+        if answer == "Student" or answer == "Professional" or answer == "Other":
+            break
+        
+        print("Please enter a valid input.")
+        
+    return answer
+
+def fam_members():
+    while True:
+        try:
+            val = int(input("Enter the number of household family members: "))
+        except:
+            print("Please enter a valid input.")
+            continue
+        break
+
+    return val
+
+def age():
+    while True:
+        try:
+            val = float(input("Enter your age: "))
+        except:
+            print("Please enter a valid input.")
+            continue
+        break
+    
+    return val
+
+def years_employed():
+    while True:
+        try:
+            val = float(input("Enter the number of years employed: "))
+        except:
+            print("Please enter a valid input.")
+            continue
+        break
+    
+    return val
+
 values = {
     'CODE_GENDER': [1 if input("Enter your gender (M/F): ") == "M" else 0],
     'FLAG_OWN_CAR': [1 if input("Do you own a car (Y/N)? ") == "Y" else 0],
     'FLAG_OWN_REALTY': [1 if input("Do you own realty (Y/N)? ") == "Y" else 0],
-    'AMT_INCOME_TOTAL': [float(input("Enter your annual income: "))],
-    'NAME_INCOME_TYPE': [input("Enter your income type (Working/Commercial associate/Other): ")],
-    'NAME_EDUCATION_TYPE': [input("Enter your education type (Secondary/Higher education/Other): ")],
-    'NAME_FAMILY_STATUS': [input("Enter your family status (Single/Married/Civil Marriage/Separated/Other): ")],
-    'NAME_HOUSING_TYPE': [input("Enter your housing type ('House / apartment'/'Rented apartment'/'Municipal "
-                                "apartment'/'With parents'/Other): ")],
+    'AMT_INCOME_TOTAL': [amount_income()],
+    'NAME_INCOME_TYPE': [income_type()],
+    'NAME_EDUCATION_TYPE': [education_type()],
+    'NAME_FAMILY_STATUS': [family_status()],
+    'NAME_HOUSING_TYPE': [housing_type()],
     'FLAG_WORK_PHONE': [1 if input("Do you have a work phone (Y/N)? ") == "Y" else 0],
     'FLAG_PHONE': [1 if input("Do you have a phone (Y/N)? ") == "Y" else 0],
     'FLAG_EMAIL': [1 if input("Do you have an email (Y/N)? ") == "Y" else 0],
-    'OCCUPATION_TYPE': [input("Enter your occupation type (Student/Professional/Other): ")],
-    'CNT_FAM_MEMBERS': [int(input("Enter the number of household family members: "))],
-    'AGE': [float(input("Enter your age: "))],
-    'YEARS_EMPLOYED': [float(input("Enter the number of years employed: "))]
+    'OCCUPATION_TYPE': [occupation_type()],
+    'CNT_FAM_MEMBERS': [fam_members()],
+    'AGE': [age()],
+    'YEARS_EMPLOYED': [years_employed()]
 }
 
 # Create a dataframe with the input values
